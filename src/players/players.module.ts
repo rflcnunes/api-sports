e488e3e87866047c84674e058e715c1589370675
rafael <rafaelcnunes@icloud.com>
2023-03-05 21:39:55 -0300

    removed tests and added exports service

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -10,5 +10,6 @@ import { Player, PlayerSchema } from './schemas/player.schema';
   ],
   controllers: [PlayersController],
   providers: [PlayersService],
+  exports: [PlayersService],
 })
 export class PlayersModule {}
